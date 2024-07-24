9777b634eba26d6b7d33fd965fb54a5b92797d9e
Favour Achara <acharafavour01@gmail.com>
2024-07-24 16:54:40 +0100

    Update page.tsx

--- app/createAccount/page.tsx
+++ app/createAccount/page.tsx
@@ -77,12 +77,14 @@ export default function Login() {
           </div>
           <p className="text-gray-500 text-[12px]">Password must contain atleast 8 characters</p>
           <div>
+            <Link href="/links">
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Create new account
             </button>
+            </Link>
           </div>
           
         </form>
